c4052e84abaae024f9bc4bc414a14e747d8b15fc
gasparuss <kacperzielinski.primary@gmail.com>
2022-01-18 21:17:23 +0100

    create Variables Description file in utils and create .env file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import 'tailwindcss/tailwind.css';
+import { useState } from 'react';
 import type { AppProps } from 'next/app';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import 'tailwindcss/tailwind.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
-
-export default MyApp;
