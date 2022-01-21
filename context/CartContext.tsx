8e4300c38a3243cda9246bc835aa44f4d4d1d17f
gasparuss <kacperzielinski.primary@gmail.com>
2022-01-21 17:56:51 +0100

    create CartContext in context folder

--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -1,2 +1,80 @@
 import { createContext, useContext, useState } from 'react';
 import type { Sofa } from '../types';
+import {
+  addProductToCart,
+  calculateTotalCartItemsCost,
+  calculateTotalCartItemsQuantity,
+  changeProductQuantity,
+  removeProductFromCart,
+} from '../utils/functions';
+
+type CartContext = {
+  cartItems: (Sofa & { quantity: number })[];
+  handleAddToCart: (product: Sofa) => void;
+  handleRemoveFromCart: (product: Sofa) => void;
+  handleChangeProductQuantity: (product: Sofa, quantity: number) => void;
+  getTotalCost: () => number;
+  getTotalQuantity: () => number;
+};
+
+const CartContext = createContext<CartContext>({
+  cartItems: [],
+  handleAddToCart: () => {},
+  handleRemoveFromCart: () => {},
+  handleChangeProductQuantity: () => {},
+  getTotalCost: () => 0,
+  getTotalQuantity: () => 0,
+});
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw new Error('Error while reading cart context!');
+  }
+
+  return context;
+};
+
+export const CartProvider = ({ children }: { children: React.ReactNode }) => {
+  const [cartItems, setCartItems] = useState<(Sofa & { quantity: number })[]>(
+    [],
+  );
+
+  const handleAddToCart = (product: Sofa) => {
+    setCartItems((cartItems) => addProductToCart(cartItems, product));
+  };
+
+  const handleRemoveFromCart = (product: Sofa) => {
+    setCartItems((cartItems) => removeProductFromCart(cartItems, product));
+  };
+
+  const handleChangeProductQuantity = (product: Sofa, quantity: number) => {
+    setCartItems((cartItems) =>
+      changeProductQuantity(cartItems, product, quantity),
+    );
+  };
+
+  const getTotalCost = () => {
+    return calculateTotalCartItemsCost(cartItems);
+  };
+
+  const getTotalQuantity = () => {
+    return calculateTotalCartItemsQuantity(cartItems);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        handleAddToCart,
+        handleRemoveFromCart,
+        handleChangeProductQuantity,
+        getTotalCost,
+        getTotalQuantity,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
