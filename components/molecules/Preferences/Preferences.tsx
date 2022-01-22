c56bf4172b87f5eb902ae95925847e5aa1a8c380
gasparuss <kacperzielinski.primary@gmail.com>
2022-01-22 16:57:04 +0100

    create RangeInput component

--- components/molecules/Preferences/Preferences.tsx
+++ components/molecules/Preferences/Preferences.tsx
@@ -1,4 +1,5 @@
 import { useProduct } from "../../../context/ProductContext";
+import { RangeInput } from "../../atoms/Input/RangeInput/RangeInput";
 import { SearchInput } from "../../atoms/Input/SearchInput/SearchInput";
 import { SelectInput } from "../../atoms/Input/SelectInput/SelectInput";
 import { StyledPreferencesWrapper } from "./Preferences.styles";
@@ -25,6 +26,12 @@ export const Preferences = () => {
       <SelectInput
         options={productsTypes.map((type) => ({ value: type, label: type }))}
         onChange={handleSelectProductTypes}
+      />
+      <RangeInput
+        price={price}
+        minPrice={minPrice}
+        maxPrice={maxPrice}
+        onChange={handleChangePrice}
       />
     </StyledPreferencesWrapper>
   );
