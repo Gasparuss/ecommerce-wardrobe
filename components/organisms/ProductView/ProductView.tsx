b87e7652d6c629a3659a388960b3f89a7c8cd606
gasparuss <kacperzielinski.primary@gmail.com>
2022-01-23 17:18:29 +0100

    create ProductView component

--- components/organisms/ProductView/ProductView.tsx
+++ components/organisms/ProductView/ProductView.tsx
@@ -1,2 +1,17 @@
-import type { Wardrobe } from "../../../types";
+import { memo } from "react";
 import { Hero } from "../Hero/Hero";
+import type { Wardrobe } from "../../../types";
+import { ProductDescription } from "../ProductDescription/ProductDescription";
+import { StyledProductViewWrapper } from "./ProductView.styles";
+
+type ProductViewProps = {
+  readonly product: Wardrobe;
+};
+
+export const ProductView = memo<ProductViewProps>(({ product }) => {
+  return (
+    <StyledProductViewWrapper>
+      <ProductDescription product={product} />
+    </StyledProductViewWrapper>
+  );
+});
