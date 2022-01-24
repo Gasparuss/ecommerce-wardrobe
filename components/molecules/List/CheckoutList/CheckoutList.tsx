89574fff758b7800cc8aec40bb6ca62cc9982e33
gasparuss <kacperzielinski.primary@gmail.com>
2022-01-24 17:08:10 +0100

    create CheckoutList component

--- components/molecules/List/CheckoutList/CheckoutList.tsx
+++ components/molecules/List/CheckoutList/CheckoutList.tsx
@@ -1,3 +1,46 @@
 import React, { memo } from "react";
 import type { Wardrobe } from "../../../../types";
 import { useCart } from "../../../../context/CartContext";
+import { StripeButton } from "../../../atoms/Button/StripeButton";
+import { CheckoutProduct } from "../../../atoms/Product/CheckoutProduct/CheckoutProduct";
+import {
+  StyledCheckoutListWrapper,
+  StyledCheckoutList,
+  StyledEmptyCheckoutList,
+  StyledCheckoutSummary,
+  StyledTotalCost,
+} from "./CheckoutList.styles";
+
+type CheckoutListProps = {
+  readonly cartItems: (Wardrobe & { quantity: number })[];
+};
+
+export const CheckoutList = memo<CheckoutListProps>(({ cartItems }) => {
+  const { getTotalCost, handleRemoveFromCart, handleChangeProductQuantity } =
+    useCart();
+
+  return (
+    <StyledCheckoutListWrapper id="checkout-list">
+      {cartItems.length ? (
+        <StyledCheckoutList>
+          {cartItems.map((cartItem) => {
+            return (
+              <CheckoutProduct
+                product={cartItem}
+                onRemoveItem={handleRemoveFromCart}
+                onChangeItemQuantity={handleChangeProductQuantity}
+                key={cartItem.id}
+              />
+            );
+          })}
+        </StyledCheckoutList>
+      ) : (
+        <StyledEmptyCheckoutList>Cart is empty</StyledEmptyCheckoutList>
+      )}
+      <StyledCheckoutSummary>
+        <StyledTotalCost>${getTotalCost()}</StyledTotalCost>
+        <StripeButton price={getTotalCost()} />
+      </StyledCheckoutSummary>
+    </StyledCheckoutListWrapper>
+  );
+});
