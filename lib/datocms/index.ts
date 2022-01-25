90cd087581340bbcfbc7b7973fe2cc674dd455d9
gasparuss <kacperzielinski.primary@gmail.com>
2022-01-25 23:37:07 +0100

    add some changes in firebase config file

--- lib/datocms/index.ts
+++ lib/datocms/index.ts
@@ -2,4 +2,10 @@ const SiteClient = require("datocms-client").SiteClient;
 
 export const DatoCMSData = new SiteClient(
   process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN,
+  {
+    headers: {
+      "content-type": "application/json",
+      authorization: `Bearer ${process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN}`,
+    },
+  },
 );
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,48 @@ import { Layout } from "../components/organisms/Layout/Layout";
 import { Main } from "../components/organisms/Main/Main";
 import { DatoCMSData } from "../lib/datocms";
 import type { Wardrobe } from "../types";
+import { gql, GraphQLClient } from "graphql-request";
 
 const Home = ({ results }: { results: Wardrobe[] }) => {
+  console.log(results);
+
   return (
-    <AuthChecker>
-      <Layout>
-        <Main results={results} />
-      </Layout>
-    </AuthChecker>
+    <Layout>
+      <Main results={results} />
+    </Layout>
   );
 };
+
+const query = gql`
+  query {
+    product {
+      id
+      name
+      cost
+      description
+    }
+  }
+`;
 
 export default Home;
 
 export const getStaticProps: GetStaticProps = async () => {
+  // const endpoint = "https://graphql.datocms.com/";
+  // const graphQLClient = new GraphQLClient(endpoint, {
+  //   headers: {
+  //     "content-type": "application/json",
+  //     authorization: "Bearer " + process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN,
+  //   },
+  // });
+  // const data = await graphQLClient.request(query);
+  // console.log(data);
+  // return {
+  //   props: { results: data },
+  // };
+
   try {
     const data = await DatoCMSData.items.all();
+
     return { props: { results: data }, revalidate: 1 };
   } catch {
     return {
