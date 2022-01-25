b6fc24288bdbdd42709ad03ce495b8985b63980e
gasparuss <kacperzielinski.primary@gmail.com>
2022-01-25 21:38:40 +0100

    changes in firebase file

--- lib/firebase/index.ts
+++ lib/firebase/index.ts
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: "AIzaSyDRnJCKnn5Dk4p235rAsppScV_P-4AH9N4",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
