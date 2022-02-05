ea1c8afb265774279a0f6f2df627bf59e2e4e96f
gasparuss <kacperzielinski.primary@gmail.com>
2022-02-05 16:04:41 +0100

    add env variables to firebase config

--- lib/firebase/index.ts
+++ lib/firebase/index.ts
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByKzvUU3XyTgNs7fR9Wq2rfTdxbebRFoY",
-  authDomain: "wardrobe-fb3ed.firebaseapp.com",
-  projectId: "wardrobe-fb3ed",
-  storageBucket: "wardrobe-fb3ed.appspot.com",
-  messagingSenderId: "630803668620",
-  appId: "1:630803668620:web:e995b496f0d2f88fbfe5a9",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 if (!firebase.apps.length) {
