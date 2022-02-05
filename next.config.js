f26b39dec2625b4c84ce1eb7db5a65d924c8794b
gasparuss <kacperzielinski.primary@gmail.com>
2022-02-05 16:58:49 +0100

    create env variables in next config file

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports = {
   images: {
     domains: ["i.ibb.co"],
   },
+  env: {
+    firebaseApiKey: process.env.FIREBASE_API_KEY,
+    firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+    firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    firebaseSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    firebaseAppId: process.env.FIREBASE_APP_ID,
+  },
 };
