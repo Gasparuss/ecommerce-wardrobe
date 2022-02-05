ffbaa2d3bc386795df61a712cddea837cbf1411a
gasparuss <kacperzielinski.primary@gmail.com>
2022-02-05 15:56:55 +0100

    add env variable to firebase config

--- lib/firebase/index.ts
+++ lib/firebase/index.ts
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByKzvUU3XyTgNs7fR9Wq2rfTdxbebRFoY",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
